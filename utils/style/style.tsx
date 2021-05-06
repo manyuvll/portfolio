4cc3c7c4397d5357f5d73c73429e9c1119693eca
manyuvll <manyuvella@gmail.com>
2021-05-07 01:28:56 +0900

    feat: pc landing page first design

---
+++ utils/style/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Column = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`
+const Row = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export { Column, Row }
